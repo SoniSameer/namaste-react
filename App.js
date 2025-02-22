6a881432bd2c91d9e16c7d0cc6ed8845329274d0
SoniSameer <sameerlink123@gmail.com>
2025-02-22 10:47:53 +0530

    second commit

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
-const heading = React.createElement(
-  'h1',
-  { id: 'heading' },
-  'Hello World from React!'
-);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// const heading = React.createElement(
+//   'h1',
+//   { id: 'heading' },
+//   'Hello World from React!'
+// );
+
+// const heading = <h1 id='heading'>Hello World from React!!!</h1>;
+
+const TitleComponent = () => <h1>This is the main title of this project</h1>;
+
+const HeadingComponent = () => {
+  return (
+    <div id='container'>
+      <TitleComponent />
+      <h1 id='heading'>Hello from the heading Component</h1>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(<HeadingComponent />);
